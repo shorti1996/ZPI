efd7701912c9551f22501d23233ea05d047cba80
Michał Karol <michal.p.karol@gmail.com>
2018-05-10 22:47:51 +0200

    Adding plotting and changing simulation

--- sim/home.py
+++ sim/home.py
@@ -3,8 +3,8 @@ from sim.world import *
 
 @static_vars(counter=0)
 class Partition(object):
-    def __init__(self, lam, size, thickness, rooms=[]):
-        self.lam = lam
+    def __init__(self, u, size, thickness, rooms=[]):
+        self.u = u
         self.counter = Partition.counter
         Partition.counter += 1
 
@@ -28,7 +28,7 @@ class Partition(object):
             self.rooms[0], self.rooms[1] = self.rooms[1], self.rooms[0]
 
         temperatureDifference = self.rooms[0].temperature - self.rooms[1].temperature
-        movingHeat = (self.lam * self.size * temperatureDifference * delta) / self.thickness
+        movingHeat = self.u * self.size * temperatureDifference * delta
 
         self.rooms[0].addHeat(-movingHeat)
         self.rooms[1].addHeat(movingHeat)
@@ -68,7 +68,23 @@ class OutsideRoom(Room):
 
     @property
     def temperature(self):
-        # TODO(mkarol) Get temperature on given moment of simulation
+        # TODO(mkarol) Get outside temperature on given moment of simulation
+        return self._temperature
+
+    @temperature.setter
+    def temperature(self, value):
+        pass
+
+    def addHeat(self, energy):
+        pass
+
+class GroundRoom(Room):
+    def __init__(self, name):
+        Room.__init__(self, name, 1)
+
+    @property
+    def temperature(self):
+        # TODO(mkarol) Get ground temperature on given moment of simulation
         return self._temperature
 
     @temperature.setter
@@ -89,22 +105,138 @@ def generateBuilding():
     # Building
     building = Building()
 
-    namesOfRoomsAndVolumes = [('RoomA', 10)]
+    roomsInitMap = {
+        '0-Area1': {
+            'volume': 10,
+        },
+        '0-Area2': {
+            'volume': 10,
+        },
+        '0-Area3': {
+            'volume': 10,
+        },
+        '0-Area4': {
+            'volume': 10,
+        },
+        '0-Area5': {
+            'volume': 10,
+        },
+        '1-Area1': {
+            'volume': 10,
+        },
+        '1-Area2': {
+            'volume': 10,
+        },
+        '1-Area3': {
+            'volume': 10,
+        },
+        '1-Area4': {
+            'volume': 10,
+        },
+        '1-Area5': {
+            'volume': 10,
+        },
+    }
+    roomsObjectMap = {}
     rooms = []
 
-    outside = OutsideRoom("Outside")
-
-    building.rooms.append(outside)
+    outside = OutsideRoom('Outside')
+    roomsObjectMap['Outside'] = outside
     rooms.append(outside)
 
-    for name, volume in namesOfRoomsAndVolumes:
-        room = Room(name, volume)
+    ground = GroundRoom('Ground')
+    roomsObjectMap['Ground'] = ground
+    rooms.append(outside)
+
+    for key in roomsInitMap:
+        room = Room(key, roomsInitMap[key]['volume'])
+        roomsObjectMap[key] = room
+
         rooms.append(room)
-        building.rooms.append(room)
 
-    rooms[1].addHeat(1000000)
-    print(rooms[1].temperature)
+    building.rooms = rooms
 
-    Partition(lam=MaterialHeatConductivity['reinforced concrete'], size=100, thickness=0.1, rooms=[rooms[0], rooms[1]])
+    roomsObjectMap['RoomN'].addHeat(1000000)
+    print(roomsObjectMap['RoomCenter'].temperature)
+
+    # 0-Area1
+    Partition(u=0.122, size=50.53, rooms=[roomsObjectMap['0-Area1'], roomsObjectMap['Outside']])
+    Partition(u=0.122, size=21.60, rooms=[roomsObjectMap['0-Area1'], roomsObjectMap['Outside']])
+    Partition(u=0.124, size=32.00, rooms=[roomsObjectMap['0-Area1'], roomsObjectMap['Outside']])
+    Partition(u=0.124, size=22.11, rooms=[roomsObjectMap['0-Area1'], roomsObjectMap['Outside']])
+    Partition(u=1.981, size=15.86, rooms=[roomsObjectMap['0-Area1']])
+    Partition(u=1.981, size=21.96, rooms=[roomsObjectMap['0-Area1']])
+    Partition(u=0.171, size=71.84, rooms=[roomsObjectMap['0-Area1'], roomsObjectMap['Ground']])
+    Partition(u=0.133, size=32.00, rooms=[roomsObjectMap['0-Area1']])
+    Partition(u=0.297, size=31.69, rooms=[roomsObjectMap['0-Area1']])
+
+    # 0-Area2
+    Partition(u=1.981, size=6.405, rooms=[roomsObjectMap['0-Area2']])
+    Partition(u=1.981, size=8.723, rooms=[roomsObjectMap['0-Area2']])
+    Partition(u=1.981, size=8.723, rooms=[roomsObjectMap['0-Area2']])
+    Partition(u=0.122, size=6.405, rooms=[roomsObjectMap['0-Area2'], roomsObjectMap['Outside']])
+    Partition(u=0.171, size=5.530, rooms=[roomsObjectMap['0-Area2'], roomsObjectMap['Ground']])
+    Partition(u=0.297, size=5.530, rooms=[roomsObjectMap['0-Area2']])
+
+    # 0-Area3
+    Partition(u=0.122, size=16.775, rooms=[roomsObjectMap['0-Area3'], roomsObjectMap['Outside']])
+    Partition(u=0.122, size=12.200, rooms=[roomsObjectMap['0-Area3'], roomsObjectMap['Outside']])
+    Partition(u=1.981, size=10.37, rooms=[roomsObjectMap['0-Area3'], roomsObjectMap['0-Area4']])
+    Partition(u=1.981, size=3.66, rooms=[roomsObjectMap['0-Area3']])
+    Partition(u=1.981, size=6.1, rooms=[roomsObjectMap['0-Area3']])
+    Partition(u=1.981, size=8.54, rooms=[roomsObjectMap['0-Area3']])
+    Partition(u=0.171, size=19.43, rooms=[roomsObjectMap['0-Area3'], roomsObjectMap['Ground']])
+    Partition(u=0.297, size=19.43, rooms=[roomsObjectMap['0-Area3']])
+
+    # 0-Area4
+    Partition(u=0.122, size=10.37, rooms=[roomsObjectMap['0-Area4'], roomsObjectMap['Outside']])
+    Partition(u=0.122, size=11.59, rooms=[roomsObjectMap['0-Area4'], roomsObjectMap['Outside']])
+    Partition(u=1.981, size=10.37, rooms=[roomsObjectMap['0-Area4']])
+    Partition(u=1.981, size=11.59, rooms=[roomsObjectMap['0-Area4']])
+    Partition(u=0.171, size=39.8025, rooms=[roomsObjectMap['0-Area4'], roomsObjectMap['Ground']])
+    Partition(u=0.297, size=39.8025, rooms=[roomsObjectMap['0-Area4']])
+
+    # 0-Area5
+    Partition(u=1.637, size=8.723, rooms=[roomsObjectMap['0-Area5']])
+    Partition(u=1.637, size=8.723, rooms=[roomsObjectMap['0-Area5']])
+    Partition(u=1.981, size=7.869, rooms=[roomsObjectMap['0-Area5']])
+    Partition(u=0.122, size=7.869, rooms=[roomsObjectMap['0-Area5'], roomsObjectMap['Outside']])
+    Partition(u=0.171, size=6.64, rooms=[roomsObjectMap['0-Area5'], roomsObjectMap['Ground']])
+    Partition(u=0.297, size=6.64, rooms=[roomsObjectMap['0-Area5']])
+
+    # Level I
+    # 1-Area1
+    Partition(u=0.122, size=14.62, rooms=[roomsObjectMap['1-Area1'], roomsObjectMap['Outside']])
+    Partition(u=0.122, size=20.61, rooms=[roomsObjectMap['1-Area1'], roomsObjectMap['Outside']])
+    Partition(u=1.981, size=14.62, rooms=[roomsObjectMap['1-Area1']])
+    Partition(u=1.981, size=15.30, rooms=[roomsObjectMap['1-Area1']])
+    Partition(u=0.133, size=17.33, rooms=[roomsObjectMap['1-Area1'], roomsObjectMap['Outside']])
+
+    # 1-Area2
+    Partition(u=0.122, size=26.01, rooms=[roomsObjectMap['1-Area2'], roomsObjectMap['Outside']])
+    Partition(u=1.981, size=14.62, rooms=[roomsObjectMap['1-Area2']])
+    Partition(u=1.981, size=21.30, rooms=[roomsObjectMap['1-Area2']])
+    Partition(u=1.981, size=14.62, rooms=[roomsObjectMap['1-Area2']])
+    Partition(u=0.133, size=14.68, rooms=[roomsObjectMap['1-Area2'], roomsObjectMap['Outside']])
+
+    # 1-Area3
+    Partition(u=1.981, size=6.83, rooms=[roomsObjectMap['1-Area3']])
+    Partition(u=1.981, size=20.40, rooms=[roomsObjectMap['1-Area3']])
+    Partition(u=1.981, size=22.50, rooms=[roomsObjectMap['1-Area3']])
+    Partition(u=0.133, size=12.24, rooms=[roomsObjectMap['1-Area3'], roomsObjectMap['Outside']])
+
+    # 1-Area4
+    Partition(u=0.122, size=8.81, rooms=[roomsObjectMap['1-Area4'], roomsObjectMap['Outside']])
+    Partition(u=1.981, size=7.78, rooms=[roomsObjectMap['1-Area4']])
+    Partition(u=1.637, size=7.78, rooms=[roomsObjectMap['1-Area4']])
+    Partition(u=1.981, size=12.57, rooms=[roomsObjectMap['1-Area4']])
+    Partition(u=0.133, size=12.59, rooms=[roomsObjectMap['1-Area4'], roomsObjectMap['Outside']])
+
+    # 1-Area5
+    Partition(u=0.122, size=11.25, rooms=[roomsObjectMap['1-Area5'], roomsObjectMap['Outside']])
+    Partition(u=0.122, size=20.54, rooms=[roomsObjectMap['1-Area5'], roomsObjectMap['Outside']])
+    Partition(u=1.981, size=11.25, rooms=[roomsObjectMap['1-Area5']])
+    Partition(u=1.981, size=13.22, rooms=[roomsObjectMap['1-Area5']])
+    Partition(u=0.133, size=25.96, rooms=[roomsObjectMap['1-Area5'], roomsObjectMap['Outside']])
 
     return building
--- sim/world_utils.py
+++ sim/world_utils.py
@@ -4,3 +4,43 @@ def milliseconds_convert(ms):
     h, m = divmod(m, 60)
     d, h = divmod(h, 24)
     return d, h, m, s
+
+
+def addPlotingBuilding(state):
+    import matplotlib.pyplot as plt
+
+    f, subplots = plt.subplots(len(state.building.rooms), 2, sharex=True)
+    xData = [0]
+    subplotsData = []
+
+    for i in range(len(subplots)):
+        subplotsData.append([[0], [0]])
+
+    f.subplots_adjust(hspace=0.5)
+
+    def animate():
+        # Add new timestep
+        xData.append(xData[len(xData) - 1] + 1)
+
+        # Add data to rooms
+        for i in range(len(state.building.rooms)):
+            subplots[i, 0].clear()
+            subplotsData[i][0].append(state.building.rooms[i].temperature)
+            subplots[i, 0].set_title(state.building.rooms[i].name)
+            subplots[i, 0].set_xlabel('time')
+            subplots[i, 0].set_ylabel('temperature')
+            subplots[i, 0].plot(xData, subplotsData[i][0], color='red')
+
+            subplots[i, 1].clear()
+            subplotsData[i][1].append(state.building.rooms[i].light)
+            subplots[i, 1].set_title(state.building.rooms[i].name)
+            subplots[i, 1].set_xlabel('time')
+            subplots[i, 1].set_ylabel('light')
+            subplots[i, 1].plot(xData, subplotsData[i][1], color='blue')
+
+        plt.draw()
+        plt.pause(0.001)
+
+    plt.draw()
+    plt.show(block=False)
+    return animate
